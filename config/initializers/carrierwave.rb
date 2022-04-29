7e5daf49a9e981f51abd58b32f34fb6801a0771a
Jae Chung <jaechung77@gmail.com>
2022-04-29 10:20:31 +1000

    setup carrierwave config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,14 @@
 CarrierWave.configure do |config|
-    config.storage    = :aws
-    config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
-    config.aws_acl    = 'public-read'
-    config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
-    config.aws_credentials = {
-      access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
-      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      region:            ENV.fetch('AWS_REGION') # Required
+
+    config.fog_credentials = {
+      provider:              'AWS',                            # required
+      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
+      aws_secret_access_key: ENV["AWS_SECRET_KEY"],            # required
+      region:                'us-east-1'                       # to match the carrierwave and bucket region
     }
+    config.fog_directory = ENV["AWS_BUCKET"]                   # required
+    config.fog_public    = false
+    config.cache_dir     = "#{Rails.root}/tmp/uploads"         # To let CarrierWave work on Heroku
+    config.storage       = :fog
+  
   end
