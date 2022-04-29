5e52ea751cc66557ac91f7c1faeeef6933fc0a54
Jae Chung <jaechung77@gmail.com>
2022-04-29 12:47:46 +1000

    added fog_provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,5 @@
 CarrierWave.configure do |config|
-    config.root = Rails.root.join('tmp') # adding these...
-    config.cache_dir = 'carrierwave' # ...two lines
+    config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider:              'AWS',                            # required
       aws_access_key_id:     ENV["AWS_ACCESS_KEY"],            # required
